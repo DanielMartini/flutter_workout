d5a5926ca416c1089fb3d5ce40d0a545a519e0f5
Rexios <rexios@rexios.dev>
2026-02-20 08:18:09 -0500

    SwiftPM migration

---
+++ ios/workout/Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "workout",
+    platforms: [
+      .iOS("13.0")
+    ],
+    products: [
+        .library(name: "workout", targets: ["workout"])
+    ],
+    targets: [
+        .target(
+            name: "workout",
+        )
+    ]
+)
